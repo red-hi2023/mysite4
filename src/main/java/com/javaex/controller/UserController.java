874d6a0eb723fb30a56fe09885e607760cc5fa78
red-hi2023 <red.hi2023@gmail.com>
2023-09-15 18:20:22 +0900

    2023-09-15

---
+++ src/main/java/com/javaex/controller/UserController.java
@@ -0,0 +1,41 @@
+package com.javaex.controller;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.javaex.dao.UserDao;
+import com.javaex.vo.UserVo;
+
+@Controller
+@RequestMapping(value="/user")
+public class UserController {
+	
+	@Autowired
+	private UserDao userDao;
+
+	//로그인폼
+	@RequestMapping(value="/loginForm", method = { RequestMethod.GET, RequestMethod.POST })
+	public String loginForm() {
+		System.out.println("UserController/loginForm()");
+
+		return "user/loginForm";
+	}
+	
+	//로그인
+	@RequestMapping(value="/login", method = { RequestMethod.GET, RequestMethod.POST })
+	public String login(@ModelAttribute UserVo userVo, HttpSession session) {
+		System.out.println("UserController/login()");
+		
+		UserVo authUser = userDao.userSelectOne(userVo);
+		
+		session.setAttribute("authUser", authUser);
+
+		return "redirect:/";
+	}
+
+}
---
+++ src/main/java/com/javaex/dao/UserDao.java
@@ -0,0 +1,25 @@
+package com.javaex.dao;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.javaex.vo.UserVo;
+
+@Repository
+public class UserDao {
+	
+	@Autowired
+	private SqlSession sqlSession;
+
+	//로그인
+	public UserVo userSelectOne(UserVo userVo) {
+		System.out.println("UserDao.UserDao()");
+
+		UserVo authUser = sqlSession.selectOne("user.selectAuthUser", userVo);
+		
+		return authUser;
+	}
+	
+	
+}
